f4efb75d2a2120879a2ba7d5e5c68bffc89fc8c0
Tibet Teixeira <tibetbrasil@gmail.com>
2018-04-17 19:55:50 -0300

    Metodo Main implementado

--- emulador.cpp
+++ emulador.cpp
@@ -1,138 +1,160 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
-#include <iostream>
 
 typedef unsigned int word;
 typedef unsigned char byte;
 typedef unsigned long int microInstrucao;
 
 byte Z , N;
-word mar, mdr, pc, sp, lv, cpp, tos, opc, h, mpc;
+word mar, mdr, pc, sp = 0, lv, cpp, tos, opc, h = 0, mpc, instruction;
 word bA, bB, bC, bD;
 byte mbr;
+
+byte barramento_read, op_ula;
+word barramento_write, next;
+int jam;
+
 microInstrucao microPrograma[512];
 byte ram[16*1024];
 byte wr, rd, fetch;
 
-	void gravar_registrador(word ender){
-		if(ender & 1) mar = bC;		//0 0000 0001
-		if(ender & 2) mdr = bC;		//0 0000 0010
-		if(ender & 4) pc = bC; 		//0 0000 0100
-		if(ender & 8) sp = bC;		//0 0000 1000
-		if(ender & 16) lv = bC;		//0 0001 0000
-		if(ender & 32) cpp = bC; 	//0 0010 0000 
-		if(ender & 64) tos = bC;	//0 0100 0000
-		if(ender & 128) opc = bC;	//0 1000 0000
-		if(ender & 256) h = bC;		//1 0000 0000
-	}
-    
-    void ler_registrador(byte ender){//endereço
-        switch(ender){
-            case 0: bB = mdr; 	break;
-            case 1: bB = pc; 	break;
-            case 2: bB = mbr; 	break;
-            case 3: //mbru = mbr com extensão de sinal
-            {
-                word sinal = mbr >> 7;
-                bB = (sinal) ? 0xFFFFFF0 : 0;
-                bB = bB | mbr;
-            }
-            					break;
-            case 4: bB = sp;	break;
-            case 5: bB = lv;	break;
-            case 6: bB = cpp; 	break;
-            case 7: bB = tos; 	break;
-            case 8: bB = opc; 	break;
-        }
-    }
+			void decode(word instruction){
+				barramento_read = (instruction << 60) >> 60;
+				op_ula = (instruction << 40) >> 56;
+				barramento_write = (instruction << 48) >> 55;
+				next = (instruction >> 27);
+				jam = (instruction << 37) >> 61;
+			}
 
-    void ula(byte operacao){
-		
-		bA = h;
+		    void ler_registrador(byte ender){//endereço
+		        switch(ender){
+		            case 0: bB = mdr; 	break;
+		            case 1: bB = pc; 	break;
+		            case 2: bB = mbr; 	break;
+		            case 3: //mbru = mbr com extensão de sinal
+		            {
+		                word sinal = mbr >> 7;
+		                bB = (sinal) ? 0xFFFFFF0 : 0;
+		                bB = bB | mbr;
+		            }
+		            					break;
+		            case 4: bB = sp;	break;
+		            case 5: bB = lv;	break;
+		            case 6: bB = cpp; 	break;
+		            case 7: bB = tos; 	break;
+		            case 8: bB = opc; 	break;
+		        }
+		    }
+			
+			void gravar_registrador(word ender){
+				if(ender & 1) mar = bC;		//0 0000 0001
+				if(ender & 2) mdr = bC;		//0 0000 0010
+				if(ender & 4) pc = bC; 		//0 0000 0100
+				if(ender & 8) sp = bC;		//0 0000 1000
+				if(ender & 16) lv = bC;		//0 0001 0000
+				if(ender & 32) cpp = bC; 	//0 0010 0000 
+				if(ender & 64) tos = bC;	//0 0100 0000
+				if(ender & 128) opc = bC;	//0 1000 0000
+				if(ender & 256) h = bC;		//1 0000 0000
+			}
+		    
 
-		byte opUla = (operacao << 2) >> 2;
-		byte desloc = operacao >> 6;
+		    void ula(byte operacao){
+				
+				bA = h;
 
-		switch(opUla){
-			case 12: bC = bA & bB;		break;
-			case 16: bC = 0;			break;
-			case 20: bC = bB;			break;
-			case 24: bC = bA;			break;
-			case 26: bC = ~bA;			break;
-			case 28: bC = bA | bB;		break;
-			case 44: bC = ~bB;			break;
-			case 49: bC = 1;			break;
-			case 50: bC = -1;			break;
-			case 53: bC = bB + 1;		break;
-			case 54: bC = bB - 1;		break;
-			case 57: bC = bA + 1;		break;
-			case 59: bC = -bA;			break;
-			case 60: bC = bA + bB;		break;
-			case 61: bC = bA + bB + 1;	break;
-			case 63: bC = bB - bA;		break;
-			default: break;
-		}
+				byte opUla = (operacao << 2) >> 2;
+				byte desloc = operacao >> 6;
 
-		if (bC == 0){
-			N = 0;
-			Z = 1;
-		}
-		else{
-			N = 1;
-			Z = 0;
-		}
+				switch(opUla){
+					case 12: bC = bA & bB;		break;
+					case 16: bC = 0;			break;
+					case 20: bC = bB;			break;
+					case 24: bC = bA;			break;
+					case 26: bC = ~bA;			break;
+					case 28: bC = bA | bB;		break;
+					case 44: bC = ~bB;			break;
+					case 49: bC = 1;			break;
+					case 50: bC = -1;			break;
+					case 53: bC = bB + 1;		break;
+					case 54: bC = bB - 1;		break;
+					case 57: bC = bA + 1;		break;
+					case 59: bC = -bA;			break;
+					case 60: bC = bA + bB;		break;
+					case 61: bC = bA + bB + 1;	break;
+					case 63: bC = bB - bA;		break;
+					default: break;
+				}
+
+				if (bC == 0){
+					N = 0;
+					Z = 1;
+				}
+				else{
+					N = 1;
+					Z = 0;
+				}
 
 
-		switch(desloc){
-			case 0:					break;
-			case 1: bC = bC >> 1; 	break;
-			case 2: bC = bC << 8; 	break;
-			default: bC = (bC << 8) >> 1;
-		}
-	}
+				switch(desloc){
+					case 0:					break;
+					case 1: bC = bC >> 1; 	break;
+					case 2: bC = bC << 8; 	break;
+					default: bC = (bC << 8) >> 1;
+				}
+			}
 
-	word next_function(word next , int jam){
+			void next_function(word next, int jam){
 
-		if (jam == 0){
-			//return next; nao precisa pois não ha caso onde 000
-		}
-		if (jam == 1)	next = next | (Z<<8);
-		if (jam == 2)	next = next | (N<<8);
-		if (jam == 4)	next = next | mbr;
-		return next;
-	}
+				if (jam == 0){
+					mpc = next;
+				}
+				// if (jam == 1)	next = next | (Z<<8);
+				// if (jam == 2)	next = next | (N<<8);
+				// if (jam == 4)	next = next | mbr;
+			}
+
+			void debug(){
+				printf("ULA %u\n", op_ula);
+
+				printf("READ %u\n", barramento_read);
+				printf("WRITE %u\n", barramento_write);
+				
+				printf("Next %u\n", next);
+				printf("JAM %d\n", jam);
+				
+				printf("SP %u\n", sp);
+				printf("H %u\n", h);
+				
+				printf("bA %u\n", bA);
+				printf("bB %u\n", bB);
+				printf("bC %u\n", bC);
+			}
 
 int main()
 {
     while(1){
+
+    	debug();
+
     	// Valores do micro programa
     	// next_address - 9 | Jam - 3 | Deslocador - 2 | F0 | F1 | En A | En B | Inv A | Inc | H | OPC | TOS | CPP | LV | SP | PC | MDR | MAR | WRITE | READ | FETCH | Barramento B - 4
 		// Barramento B
 		// MDR - 0 | PC - 1 | MBR - 2 | MBRU - 3 | SP - 4 | LV - 5 | CPP - 6 | TOS - 7 | OPC - 8 
 		 
-		// microPrograma[teste] = 000000110 000 00000000 000000000 000 0010;
+		// microPrograma[teste] = 0b000000010 000 00000000 000000000 000 0000;
 
-		microPrograma[0] = 0b000000010000000000000000000000000000;
+		microPrograma[0] = 0b000000010000000000000000000000000000; // Next address = 2
 
-		microPrograma[2] = 0b000000011000001110010000010000000100;
-		microPrograma[3] = 0b000000100000001110010000010000000100;
-		microPrograma[4] = 0b000000101000001110010000010000000100;
-		microPrograma[5] = 0b000000110000001110010000010000000100;
+		microPrograma[2] = 0b000000010000001101010000010000000100; // Next 2; bB+1; Write SP; Read SP
 
-		microPrograma[6] = 0b000000111000000000000000100000000100;
-		microPrograma[7] = 0b000000110000000000000000000000000010;
-
-
-		byte ender_read = (microPrograma[0] << 32) >> 32;
-		byte oper = (microPrograma[0] << 12) >> 28;
-		word ender_write = (microPrograma[0] << 20) >> 27;
-		word next = (microPrograma[0] >> 27);
-		int jam = (microPrograma[0] << 9) >> 33;
-
-		ler_registrador(ender_read);
-		ula(oper);
-		gravar_registrador(ender_write);
+		instruction = microPrograma[mpc];
+		decode(instruction);
+	
+		ler_registrador(barramento_read);
+		ula(op_ula);
+		gravar_registrador(barramento_write);
 		next_function(next, jam);
 
 		getchar();
---
+++ teste.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+typedef unsigned int word; //8bits
+typedef unsigned char byte; //32bits
+typedef unsigned long int microInstrucao; // 64bits
+
+
+int main(int argc, char const *argv[])
+{
+	// microInstrucao teste = 0b000000011 000 00010100 100000000 000 0100; 36bits 
+	microInstrucao teste = 0b000000011000000101001000000000000100; 
+	// microInstrucao teste = 0b0001;
+	// microInstrucao teste = 536870912;
+	
+	int endereco1 = ((teste << 40) >> 56);
+	// int endereco2 = ((teste << 12) >> 28);
+	printf("%d\n", endereco1);
+
+	return 0;
+}
