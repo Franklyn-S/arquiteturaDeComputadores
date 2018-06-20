46387253ac3ce1e8b067c8ba3cc4c484673533eb
Franklyn Seabra <franklynseabra@alu.ufc.br>
2018-06-20 11:55:35 -0300

    Deu certo essa bagaça, vlw Icaro S2

--- ExerciciosAP2/usr/bin/strings.asm
+++ ExerciciosAP2/usr/bin/strings.asm
@@ -4,32 +4,23 @@
 		cli
 
 		push 0; para parar o loop
-		
+		mov dh, 1; para printar na linha 1
 
 loop1:	mov ah, 0
 		int 0x16
-		mov ah, 0x0e
-		int 0x10
-
 		cmp al, 13
 		je print
-
+		mov ah, 0x02
+		int 0x10
 		push ax
-		
 		jmp loop1	
 
 print:
-		mov al, 10
-		mov ah, 0x0e
-		int 0x10
-		;mov dh, 1; para printar na linha 1
-loop2:
 		pop ax
-		mov ah, 0x0E
 		int 0x10
-		or al, al
+		cmp al, al
 		jz fim
-		jmp loop2
+		jmp print
 
 fim:	hlt
 
